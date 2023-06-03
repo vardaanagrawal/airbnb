a403970826b88acad8a81829d3f87a6bf6c54505
Vardaan <vardaanagrawal517@gmail.com>
2023-06-03 12:52:06 +0530

    added maps page

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -3,10 +3,12 @@ import Setup from "./setup/Setup";
 import Protection from "./protection/Protection";
 import Footer from "./footer/Footer";
 import Question from "./questions/Question";
+import Map from "./map/Map";
 
 export default function Home() {
   return (
     <div className="home">
+      <Map />
       <Setup />
       <Protection />
       <Question />
---
+++ src/components/home/map/Map.js
@@ -0,0 +1,51 @@
+import React, { useMemo, useState } from "react";
+import "./map.css";
+import { GoogleMap, useLoadScript } from "@react-google-maps/api";
+
+const api = "AIzaSyCZQdWZWsNyakL30EbvVherjO4c9HcqFc8";
+
+export default function Map() {
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: api,
+  });
+
+  const [data, setData] = useState(15);
+
+  const center = useMemo(() => ({ lat: 28.6, lng: 77 }), []);
+
+  return (
+    <div className="map-section">
+      <div className="map-info">
+        <div className="map-title">Airbnb it.</div>
+        <div className="map-desc">You could earn</div>
+        <div className="map-price">₹{data * 3129}</div>
+        <div className="map-nights">
+          <span className="night-count">{data} nights</span> at an estimated
+          3129 a night
+        </div>
+        <div className="map-slider">
+          <input
+            type="range"
+            min={1}
+            max={30}
+            value={data}
+            onChange={(e) => {
+              setData(e.target.value);
+            }}
+          ></input>
+        </div>
+      </div>
+      <div className="map-box">
+        {isLoaded ? (
+          <GoogleMap
+            mapContainerClassName="map"
+            zoom={10}
+            center={center}
+          ></GoogleMap>
+        ) : (
+          "Loading"
+        )}
+      </div>
+    </div>
+  );
+}
